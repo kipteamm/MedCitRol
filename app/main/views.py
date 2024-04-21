8f71b384ff6f29b11b8c163e027b6b967e81ed98
kipteam <toro.een@gmail.com>
2024-04-21 16:11:48 +0200

    Updated reset

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, redirect, send_from_directory, req
 from app.utils.functions import get_access_key
 from app.teacher.models import Task, TaskField, TaskOption
 from app.game.models import World, Settlement, SettlementRuler, Character, AccessKey, Tile, InventoryItem, Farmer, MarketItem, Merchant
+from app.auth.models import UserWorlds
 from app.extensions import db
 
 import os
@@ -23,44 +24,20 @@ def help():
 
 @main_blueprint.route('/reset')
 def reset():
-    for task in Task.query.all():
-        db.session.delete(task)
-
-    for task_field in TaskField.query.all():
-        db.session.delete(task_field)
-
-    for task_option in TaskOption.query.all():
-        db.session.delete(task_option)
-
-    for world in World.query.all():
-        db.session.delete(world)
-
-    for settlement in Settlement.query.all():
-        db.session.delete(settlement)
-
-    for settlement_ruler in SettlementRuler.query.all():
-        db.session.delete(settlement_ruler)
-
-    for character in Character.query.all():
-        db.session.delete(character)
-
-    for access_key in AccessKey.query.all():
-        db.session.delete(access_key)
-
-    for tile in Tile.query.all():
-        db.session.delete(tile)
-
-    for item in InventoryItem.query.all():
-        db.session.delete(item)
-
-    for farmer in Farmer.query.all():
-        db.session.delete(farmer)
-
-    for item in MarketItem.query.all():
-        db.session.delete(item)
-
-    for merchant in Merchant.query.all():
-        db.session.delete(merchant)
+    Task.query.delete()
+    TaskField.query.delete()
+    TaskOption.query.delete()
+    World.query.delete()
+    Settlement.query.delete()
+    SettlementRuler.query.delete()
+    Character.query.delete()
+    AccessKey.query.delete()
+    Tile.query.delete()
+    InventoryItem.query.delete()
+    Farmer.query.delete()
+    MarketItem.query.delete()
+    Merchant.query.delete()
+    UserWorlds.query.delete()
 
     db.session.commit()
 
